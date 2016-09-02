b6ea8487784862e5fe90a30bfc7fd3ee139b925f
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2016-09-02 18:06:05 -0500

    refactor angles function

--- triplets/angles.py
+++ triplets/angles.py
@@ -1,35 +1,20 @@
 from __future__ import division
+import cProfile
 import numpy as np
 from numpy import linalg as LA
 
-def getAngle1(V1, V2):
-    '''
-    This works
-    arccos( (V1 dot V2) / ( norm(V1) * norm(v2) ))
-    '''
+def get_angle(V1, V2):
     dot = np.vdot(V1, V2)
     angle = np.arccos( dot / (LA.norm(V1) * LA.norm(V2)) )
     return np.degrees(angle)
 
-def getAngle2(V1, V2):
-    '''
-    arcsin( (V1 cross V2) / ( norm(V1) * norm(v2) ) )
-    '''
-    cross = np.cross(V1, V2)
-    angle = np.arcsin( cross / (LA.norm(V1) * LA.norm(V2)) )
-    return np.degrees(angle)
-
 def main():
-    # A = [2, 5]
-    # B = [7, 2]
-    # C = [2, 2]
     A = [5, 7]
     B = [10, 5]
     C = [5, 5]
     V1 = np.subtract(A, C)
     V2 = np.subtract(B, C)
-    print "With arcsin", getAngle2(V1, V2)
-    print "With arccos", getAngle1(V1, V2)
+    print "Angle : {0}".format(get_angle(V1, V2))
 
 if __name__ == '__main__':
     main()
