b57b2b7a8d89b33110972ee0f557c9746c728ab7
John Osorio <john@sirius.utp.edu.co>
2017-03-09 14:49:00 -0500

    Se adicionan algunos campos.

--- cuda/d_match.hpp
+++ cuda/d_match.hpp
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <cuda.h>
 
+#define SIZE 3
+
+
 __global__ int d_hyperedges (int *edges1, int *edges2,
         d_key_points *kp1, d_key_points *kp,
         d_Mat *desc1, d_Mat *desc2, double c1,
@@ -16,7 +19,25 @@ __global__ int d_hyperedges (int *edges1, int *edges2,
     int i = blockIdx.y*blocDim.y + threadIdx.y;
     int j = blockIdx.y*blocDim.y + threadIdx.y;
 
-    if (i < edges1Size)
+    if (i < edges1Size){
+        int best_match_idx = -1;
+        double max_similarity = -1E30;
+        double s_ang = -1E30;
+        double s_ratios = -1E30;
+        if(j < edges2Size){
+            d_Point2f e1_points[3], e2_points[3];
+            d_Mat des_1[3], des_2[3];
+            #pragma unroll
+            for (int k = 0; k < SIZE; k++){
+                e1_points[k].x = kp1[edges1[i*WIDTH_EDGES+k]].x;
+                e1_points[k].y = kp1[edges1[i*WIDTH_EDGES+k]].y;
+                e2_points[k].x = kp2[edges2[i*WIDTH_EDGES+k]].x;
+                e2_points[k].y = kp2[edges2[i*WIDTH_EDGES+k]].y;
+
+
+            }
+        }
+    }
 
 }
 
